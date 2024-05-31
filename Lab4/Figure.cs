ac2d3460972a039973cccc7c9e40c014ebe01bb4
GhostInn <112103443+GhostInn@users.noreply.github.com>
2024-05-31 22:54:18 +0300

    Update Figure.cs

--- Lab4/Figure.cs
+++ Lab4/Figure.cs
@@ -1,8 +1,6 @@
 namespace Lab4;
 
-// Объявляем абстрактный класс Figure
 public abstract class Figure
 {
-    // Объявляем абстрактный метод CalculateArea, который должен быть реализован в дочерних классах
     public abstract double CalculateArea();
 }
